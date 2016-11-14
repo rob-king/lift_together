62e8dfcace07cf653ddefc68a36df8b78a4a28cf
robert <robert.k25@gmail.com>
2016-11-14 10:49:52 -0500

    adjusts validation and how errors are displayed

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,4 +1,6 @@
 class Campaign < ApplicationRecord
-    validates :name, :organization, :description, :end_date, :goal_amount, presence:true
+    validates :end_date, :goal_amount, presence:true
+    validates :description, length: {minimum: 30}, presence: true
+    validates :name, :organization, length: {minimum: 3}, presence: true
     belongs_to :user
 end
