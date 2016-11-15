d954a6ee1a4deeb8488682662ce93002db802687
robert <robert.k25@gmail.com>
2016-11-15 12:06:34 -0500

    adds validation for pledge and destroy dependents

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -3,5 +3,5 @@ class Campaign < ApplicationRecord
     validates :description, length: {minimum: 30}, presence: true
     validates :name, :organization, length: {minimum: 3}, presence: true
     belongs_to :user
-    has_many :pledges
+    has_many :pledges, dependent: :destroy
 end
--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,4 +1,5 @@
 class Pledge < ApplicationRecord
+  validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 5 }
   belongs_to :campaign
   belongs_to :user
 end
